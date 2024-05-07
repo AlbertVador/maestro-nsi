23bc6a1aa34492b0f2c1eed66fc5052273060e74
manndonk <manndonK@gmail.com>
2024-05-06 20:30:53 -0400

    Revert "Update main.js"
    This reverts commit bb3408d8aa74055967a5c38cda94bfdffbfdc4f7.

--- Server/pages/public/ordi/main.js
+++ Server/pages/public/ordi/main.js
@@ -1,75 +1,95 @@
-// Suivi des performances du joueur
+document.onkeydown = (e) => {
+    switch (e.key) {
+        case "a":
+            arrows[0].push(new Arrow(0, -50));
+            break;
+        case "s":
+            arrows[1].push(new Arrow(1, -50));
+            break;
+        case "d":
+            arrows[2].push(new Arrow(2, -50));
+            break;
+        case "f":
+            arrows[3].push(new Arrow(3, -50));
+            break;
+        case "v":
+            rateAction(0);
+            break;
+        case "b":
+            rateAction(1);
+            break;
+        case "n":
+            rateAction(2);
+            break;
+        case "m":
+            rateAction(3);
+            break;
+    }
+}
+
 let performanceJoueur = {
     "parfait" : 0,
     "bien": 0
-};
-
-// Fonction pour évaluer l'action du joueur sur une voie spécifique
+}
 const rateAction = (lane) => {
     if (arrows[lane].length === 0) {
         console.log("Aucune flèche");
         return 0;
     }
-
-    // Calcul de la différence entre la position de la flèche et la position cible
     const altitude = arrows[lane][0].y;
     const difference = Math.abs(canvasHeight/2 - altitude - 25);
-
-    // Suppression de la première flèche de la voie active et ajout dans les flèches mortes
     arrows[lane].splice(0, 1);
     arrows[4].push(new Arrow(lane, altitude, true));
-
-    // Évaluation de la précision de l'action et mise à jour des performances du joueur
     if (difference <= 20) {
         console.log("Parfait!");
         performanceJoueur["parfait"]++;
-        lineColour = "green";
+        lineColour = "green"
+        renderStatic()
     }
     else if (difference <= 60) {
-        console.log("Bien.");
+        console.log("Bien.")
         performanceJoueur["bien"]++;
-        lineColour = "orange";
+        lineColour = "orange"
+        renderStatic()
     }
     else {
         console.log("Mauvais :(");
-        lineColour = "red";
+        lineColour = "red"
+        renderStatic()
     }
-};
+}
 
-// Fonction appelée lorsque le jeu est terminé
+let arrowCount = 0;
+const titre = document.getElementById("titre");
+const conteneurGameOver = document.getElementById("gameOver");
+const chiffresScore = document.getElementsByClassName("chiffreScore");
+const bouttonRejouer = document.getElementById("rejouer");
 const gameOver = () => {
-    // Affichage des éléments de fin de jeu
     canvas.style.display = "none";
     titre.style.display = "block";
     conteneurGameOver.style.display = "block";
     bouttonRejouer.style.display = "block";
-
-    // Calcul du score du joueur en fonction des performances
-    const score = Math.floor(100000 * (performanceJoueur["parfait"] + 0.75 * performanceJoueur["bien"]) / arrowCount); // 100000 = maximum possibles de points
+    const score = Math.floor(100000* (performanceJoueur["parfait"] + 0.75 * performanceJoueur["bien"]) / arrowCount); // 100000 = maximum possibles de points
     console.log(score);
-
-    // Animation des chiffres du score
     setTimeout(() => {
         for (i = 5; i >= 0; i--) {
             chiffresScore[i].style.translate = `0rem ${-Math.floor(score/(10**(5-i))%10)*6 - 0.5}rem`;
         }
     }, 1500);
-};
+}
 
-// Gestion du clic sur le bouton "Rejouer"
 bouttonRejouer.onclick = () => {
-    // Réinitialisation des éléments pour une nouvelle partie
     bouttonRejouer.style.display = "none";
     conteneurGameOver.style.display = "none";
     conteneurChoixChanson.style.display = "block";
-    lineColour = "black";
+    lineColour = "black"
     chansonFinit = false;
     arrowCount = 0;
     arrows.forEach(lane => lane.length = 0);
     performanceJoueur = {
         "parfait" : 0,
         "bien": 0
-    };
+    }
     for (i = 0; i < 6; i++) {
         chiffresScore[i].style.translate = "0rem -0.5rem";
     }
@@ -78,42 +98,79 @@ bouttonRejouer.onclick = () => {
     audioContext.close();
     audioContext = new AudioContext();
     audioContext.suspend();
-};
+}
 
-// Fonction de rendu de la ligne centrale et de l'arrière-plan
-const renderStatic = () => {
-    arrierePlan.render(); // Affichage de l'arrière-plan
+let arrierePlan;
+let lineColour = "black"
+const renderStatic = () => { // Afficher la ligne au millieu
+    arrierePlan.render();
     ctx.lineWidth = 2;
     const firstLaneX = canvasWidth/2 - 2 * laneWidth;
     ctx.strokeStyle = lineColour;
     ctx.beginPath();
+    // for (let i = 0; i < 5; i++) {
+    //     ctx.moveTo(firstLaneX + i*laneWidth, 0);
+    //     ctx.lineTo(firstLaneX + i*laneWidth, canvasHeight);
+    // }
     ctx.moveTo(firstLaneX, canvasHeight/2);
     ctx.lineTo(firstLaneX + 4*laneWidth, canvasHeight/2);
     ctx.stroke();
     ctx.closePath();
-};
+}
 
-// Initialisation de la liste des flèches (actives et mortes)
-const arrows = [[], [], [], [], []]; // 4 premieres listes pour les 4 lignes, 5eme pour toutes les fleches mortes
+const arrows = [[], [], [], [], []]; // Une liste de 5 listes: les 4 premières pour les flèches actives, et la dernière pour les flèches mortes (en gris)
+const renderArrows = (dt) => {
+    for (i = 0; i < 4; i++) {
+        if (arrows[i].length > 0 && canvasHeight/2 - arrows[i][0].y - 25 < -60) {
+            const altitude = arrows[i][0].y;
+            arrows[i].splice(0, 1);
+            arrows[4].push(new Arrow(i, altitude, true));
+            arrows[4][arrows[4].length - 1].render();
+        }
+        for (j=arrows[i].length - 1; j >= 0; j--) {
+            arrows[i][j].updatePosition(dt);
+            arrows[i][j].render();
+        }
+    }
+    for (i=arrows[4].length - 1; i >= 0; i--) {
+        arrows[4][i].updatePosition(dt);
+        if (arrows[4][i].y > canvasHeight) {
+            arrows[4].splice(i, 1);
+        }
+        else {
+            arrows[4][i].render();
+        }
+    }
+}
 
-// Boucle principale de jeu
+const groupes = [];
+const renderMusiciens = (dt, temps) => {
+    groupes.forEach(groupe => {
+        groupe.update(dt, temps);
+    });
+}
+
+let chansonFinit = false;
+const statusToLane = ["rien", 2, 1, 3, 0];
+let tempsFleches;
+let timestampCount = 0;
+let lastDirection = null;
+const arrowSpeed = 200/1000; // px/ms
+const arrowDelay = (canvasHeight + 25) / (2*arrowSpeed); // v = d / t | t = d / v
+let previousTime = performance.now();
 const gameLoop = (currentTime) => {
     dt = currentTime - previousTime;
     previousTime = currentTime;
-
-    // Gestion du timing des flèches
     if (timestampCount < tempsFleches.length) {
-        // Ajout des nouvelles flèches selon le temps de la chanson
-        if (audioContext.currentTime * 1000 + arrowDelay >= tempsFleches[timestampCount]["temps"]) {
+        if (audioContext.currentTime*1000 + arrowDelay >= tempsFleches[timestampCount]["temps"]) {
             for (i = 0; i < Math.min(nombreTelephones, tempsFleches[timestampCount]["directions"].length); i++) {
                 arrows[tempsFleches[timestampCount]["directions"][i]].push(new Arrow(tempsFleches[timestampCount]["directions"][i], -50));
                 arrowCount++;
             }
-            // Génération aléatoire de flèches si aucune direction n'est spécifiée
             if (tempsFleches[timestampCount]["directions"].length == 0) {
                 let dir = [1,2,3,4];
-                dir.splice(lastDirection-1,1);
-                dir = dir[Math.floor(Math.random()*3)];
+                dir.splice(lastDirection-1,1)
+                dir = dir[Math.floor(Math.random()*3)]
                 arrows[statusToLane[dir]].push(new Arrow(statusToLane[dir], -50));
                 arrowCount++;
                 lastDirection = dir;
@@ -121,23 +178,18 @@ const gameLoop = (currentTime) => {
             timestampCount++;
         }
     }
-
-    // Effacement du canvas et rendu des éléments du jeu
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    renderStatic();
+    renderStatic(); // Changer la couleur de la ligne au millieu
     renderMusiciens(dt, audioContext.currentTime);
     renderArrows(dt);
-
-    // Vérification de la fin de la chanson
     if (chansonFinit) {
         gameOver();
-    } else {
-        requestAnimationFrame(gameLoop); // Appel récursif pour la prochaine frame
     }
-};
+    else {
+        requestAnimationFrame(gameLoop);
+    }
+}
 
-
-// Initialisation des variables liées au WebSocket et aux éléments HTML
 let webSocket;
 let nombreTelephones = 0;
 const bouttonCommencer = document.getElementById("commencer");
@@ -146,58 +198,39 @@ const conteneur = document.getElementById("conteneur");
 const divsEtats = [document.getElementById("etat1"), document.getElementById("etat2")];
 const bouttonJouer = document.getElementById("jouer");
 const conteneurConnect = document.getElementById("conteneurConnect");
-
-// Gestion du clic sur le bouton "Commencer"
 bouttonCommencer.onclick = () => {
-    // Suppression des éléments de règles et du bouton "Commencer"
     conteneur.removeChild(document.getElementById("regles"));
     conteneur.removeChild(bouttonCommencer);
-
-    // Ouverture d'une connexion WebSocket avec le serveur
-    webSocket = new WebSocket(`wss://${window.location.hostname}:8008`);
-    
-    // Gestion des messages reçus sur le WebSocket
+    webSocket = new WebSocket(`wss://${window.location.hostname}:8008`); // ouvrir le websocket, quand on l'ouvre on reçoit un code
     webSocket.onmessage = (reponse) => {
-        // Mise à jour de l'élément HTML avec le code reçu du serveur
         codeConnect.innerHTML = reponse.data;
-        
-        // Gestion des messages ultérieurs sur le WebSocket
         webSocket.onmessage = (infoTelephone) => {
             if (infoTelephone.data === "connecté") {
-                // Mise à jour de l'état du téléphone connecté
                 divsEtats[nombreTelephones].innerHTML = "Lié";
                 nombreTelephones++;
-            } else if (infoTelephone.data === "déconnecté") {
-                // Mise à jour de l'état du téléphone déconnecté
+            }
+            else if (infoTelephone.data === "déconnecté") {
                 nombreTelephones--;
                 divsEtats[nombreTelephones].innerHTML = "...";
             }
-            // Affichage ou non du bouton "Jouer" en fonction du nombre de téléphones connectés
             bouttonJouer.style.display = nombreTelephones > 0 ? "block" : "none";
-        };
-    };
-
-    // Affichage du conteneur de connexion
+        }
+    }
     conteneurConnect.style.display = "block";
-};
+}
 
-// Gestion du clic sur le bouton "Jouer"
+const conteneurChoixChanson = document.getElementById("conteneurChoixChanson");
 bouttonJouer.onclick = () => {
-    // Affichage du conteneur de choix de chanson et masquage des autres éléments
     conteneurConnect.style.display = "none";
     bouttonJouer.style.display = "none";
     conteneurChoixChanson.style.display = "block";
-
-    // Gestion des messages reçus sur le WebSocket lors du jeu
-    webSocket.onmessage = (message) => {
-        // Vérification du message reçu et appel de la fonction rateAction en conséquence
+    webSocket.onmessage = (message) => { // on définit quand on reçoit un message
         if (message.data != "rien") {
-            rateAction(message.data); // Le message contient la direction de l'action
+            rateAction(message.data); // le message contient la direction
         }
-    };
-};
+    }
+}
 
-// Fonction pour jouer un fichier audio
 let audioContext = new AudioContext();
 let source;
 const joueAudio = async (nomAudio) => {
@@ -206,32 +239,22 @@ const joueAudio = async (nomAudio) => {
       .then(res => res.arrayBuffer())
       .then(ArrayBuffer => audioContext.decodeAudioData(ArrayBuffer));
 
-    // Connexion du buffer audio à la destination de l'AudioContext
     source.buffer = audioBuffer;
     source.connect(audioContext.destination);
-
-    // Déclenchement de l'événement lorsque l'audio se termine
     source.onended = () => {
         chansonFinit = true;
-    };
-
-    // Démarrage de la lecture audio
+    }
     source.start();
-    audioContext.resume(); // Réactivation du contexte audio si nécessaire
+    audioContext.resume();
 };
 
-// Fonction pour commencer le jeu avec une chanson spécifique
 const commenceJeu = async (fichierChanson) => {
-    // Récupération des données de la chanson depuis le serveur
     const reponse = await fetch(`/chanson/${fichierChanson}`);
     const json = await reponse.json();
-    
-    // Initialisation des données de jeu (positions des musiciens, temps des flèches, etc.)
     tempsFleches = json["fleches"];
     arrierePlan = new Sprite("imagesJeu/Stage.png", 0, 0, 64, 36, 0, 0, canvasWidth, canvasHeight);
     const infoInstruments = json["instruments"];
-    
-    // Création des groupes de musiciens en fonction des données récupérées
+    // Placements des joueurs :
     [
         ["trompette", [[275, 220], [200, 250], [125, 300], [660, 120, true], [770, 150, true], [880, 190, true]], [80, 160]],
         ["cor", [[640, 230], [725, 250], [810, 280]], [105, 160]],
@@ -242,44 +265,30 @@ const commenceJeu = async (fichierChanson) => {
     ].forEach(element => {
         const musiciens = [];
         element[1].forEach(musicien => {
-            // Création et ajout des musiciens dans chaque groupe
             musiciens.push(new Musicien(`/imagesJeu/musiciens/${element[0]}${musicien[2] ? "Flipped" : ""}/${Math.floor(Math.random() * 4)}.png`, element[0], musicien[0], musicien[1], element[2][0], element[2][1]));
         });
-        // Création et ajout des groupes dans le jeu
         groupes.push(new Groupe(musiciens, Object.keys(infoInstruments).includes(element[0]) ? infoInstruments[element[0]] : []));
     });
-
-    // Lecture de la chanson audio
     joueAudio(json["fichier"]);
-
-    // Début de la boucle principale du jeu
     requestAnimationFrame(gameLoop);
-};
+}
 
-// Préparation de la liste des chansons disponibles
 const listeChansons = document.getElementById("listeChansons");
 const preparerListeChansons = async () => {
     const reponse = await fetch("/chansonsPossibles");
     const chansons = await reponse.json();
-    
-    // Création des éléments de liste pour chaque chanson disponible
     chansons.forEach(chanson => {
         const element = document.createElement("div");
         element.classList.add("chanson");
         element.innerHTML = chanson[0];
 
-        // Gestion du clic sur une chanson pour commencer le jeu
         element.onclick = () => {
             canvas.style.display = "block";
             conteneurChoixChanson.style.display = "none";
             titre.style.display = "none";
             commenceJeu(chanson[1]);
-        };
-
-        // Ajout de l'élément à la liste des chansons affichées
+        }
         listeChansons.appendChild(element);
-    });
-};
-
-// Appel de la fonction pour préparer la liste des chansons
+    })
+}
 preparerListeChansons();
