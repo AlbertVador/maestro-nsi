6a9adb0ac9504797e05cf0053f21a96dbc37e819
manndonk <manndonK@gmail.com>
2024-05-06 20:30:45 -0400

    Revert "Update index.js"
    This reverts commit 5f7c8887534238ff329ab24a65f9bfa882d6462f.

--- Server/index.js
+++ Server/index.js
@@ -1,40 +1,27 @@
-// Import des modules nécessaires
-import * as path from "path"; // Pour gérer les chemins de fichiers
-import express from "express"; // Framework pour les applications web en Node.js
-import cors from "cors"; // Middleware pour gérer les requêtes CORS
-import * as url from "url"; // Pour analyser les URL
-
-// Initialisation de l'application Express
+import * as path from "path";
+import express from "express";
+import cors from "cors";
+import * as url from "url";
 const app = express();
 
-// Import des middlewares
-import bodyParser from "body-parser"; // Middleware pour analyser le corps des requêtes HTTP
-import device from 'express-device'; // Middleware pour détecter le type de dispositif
+import bodyParser from "body-parser" // npm install body-parser
+import device from 'express-device'; // npm install express-device
 
-// Utilisation des middlewares
-app.use(bodyParser()); // Analyse le corps des requêtes en JSON, texte brut, ou URL encodées
-app.use(device.capture()); // Capture le type de dispositif (ordinateur, téléphone, etc.)
+app.use(bodyParser());
+app.use(device.capture());
 
-// Import des modules WebSocket et système de fichiers
-import * as ws from "ws"; // Pour les WebSockets
-import * as fs from "fs"; // Pour gérer les fichiers système
-
-// Création des serveurs WebSocket sur différents ports
-const clientServer = new ws.WebSocketServer({ port: 8008 }); // WebSocket pour les clients
-const phoneServer = new ws.WebSocketServer({ port: 8080 }); // WebSocket pour les téléphones
-
-// Port d'écoute pour le serveur Express
+import * as ws from "ws";
+import * as fs from "fs";
+const clientServer = new ws.WebSocketServer({ port: 8008 });
+const phoneServer = new ws.WebSocketServer({ port: 8080 });
 const port = 3000;
 
-// Configuration des middlewares
-app.use(cors()); // Autorise les requêtes CORS (Cross-Origin Resource Sharing)
-app.use(express.static(path.join("./pages/public"))); // Définit le dossier des fichiers statiques (CSS, JS, etc.)
+app.use(cors());
+app.use(express.static(path.join("./pages/public")));
 
-// Objets de stockage des connexions WebSocket
-const phoneSockets = {}; // Stocke les connexions des téléphones
-const clientSockets = {}; // Stocke les connexions des clients (ordinateurs)
+const phoneSockets = {};
+const clientSockets = {};
 
-// Fonction pour générer un identifiant unique
 const createUniqueID = () => {
     return String.fromCharCode(
         65 + Math.floor(Math.random()*26),
@@ -45,93 +32,77 @@ const createUniqueID = () => {
     );
 }
 
-// Gestion des connexions WebSocket des clients (ordinateurs)
 clientServer.on("connection", (ws) => {
     let sID = createUniqueID();
     while (Object.keys(clientSockets).includes(sID)) {
         sID = createUniqueID();
     }
-    // Stockage de la connexion client avec son identifiant et un compteur d'appareils connectés
+    console.log("code : ", sID)
     clientSockets[sID] = [ws, 0];
-    console.log("code : ", sID);
     console.log(Object.keys(clientSockets).length);
     console.log("connected");
-
-    // Gestion de la déconnexion du client
+    ws.on("message", (data) => {})
     ws.on("close", () => {
         delete clientSockets[sID];
-        // Fermeture des connexions téléphoniques associées
         for (const [key, value] of Object.entries(phoneSockets)) {
             if (value[1] === sID) {
                 phoneSockets[key][0].close();
             }
         }
         console.log("closed");
-    });
-
-    // Envoi de l'identifiant unique au client
+    })
     ws.send(sID);
-});
+})
 
-// Gestion des connexions WebSocket des téléphones
 phoneServer.on("connection", (ws, req) => {
-    // Extraction du code d'identification depuis l'URL
     const parametresUrl = url.parse(req.url, true);
     const code = parametresUrl.query.code;
-
-    // Vérification du code d'identification et du nombre maximal d'appareils connectés
     if (Object.keys(clientSockets).includes(code) && clientSockets[code][1] < 2) {
         let sID = createUniqueID();
         while (Object.keys(phoneSockets).includes(sID)) {
             sID = createUniqueID();
         }
 
-        // Stockage de la connexion téléphone avec son identifiant et le code client associé
         phoneSockets[sID] = [ws, code];
-        clientSockets[code][1]++; // Incrémentation du nombre d'appareils connectés au client
-
-        // Envoi de message au client pour indiquer la connexion d'un téléphone
+        clientSockets[code][1]++;
         clientSockets[code][0].send("connecté");
-
-        // Gestion des messages reçus du téléphone
         ws.on("message", (data) => {
             const statutTel = parseInt(data.toString());
             console.log(statutTel);
-            clientSockets[code][0].send(statutTel); // Transmission du statut au client
-        });
+            clientSockets[code][0].send(statutTel);
+        })
     
-        // Gestion de la déconnexion du téléphone
         ws.on("close", () => {
             delete phoneSockets[sID];
             if (Object.keys(clientSockets).includes(code)) {
                 clientSockets[code][0].send("déconnecté");
-                clientSockets[code][1]--; // Décrémentation du nombre d'appareils connectés au client
+                clientSockets[code][1]--;
             }
             console.log("closed");
-        });
-
-        // Envoi d'un message de succès au téléphone
+        })
         ws.send("succes");
-    } else {
-        ws.close(); // Fermeture de la connexion si le code n'est pas valide ou le client a atteint le nombre maximal d'appareils connectés
     }
-});
+    else {
+        ws.close();
+    }
+})
 
-// Chemin pour renvoyer la page principale du jeu en fonction du type de dispositif
+// Chemin pour envoyer la page principale du jeu
 app.get("/", (req, res) => {
     if (req.device.type == 'phone') {
-      res.sendFile("telephone.html", { root: "pages" }); // Envoi de la page pour les téléphones
-    } else {
-      res.sendFile("ordinateur.html", { root: "pages" }); // Envoi de la page pour les ordinateurs
+      res.sendFile("telephone.html", { root: "pages" })
+    }
+    else {
+      res.sendFile("ordinateur.html", { root: "pages" });
     }
 });
 
-// Chemin pour renvoyer la page pour les téléphones
+// Chemin pour envoyer la page telephone
 app.get("/telephone", (req, res) => {
     res.sendFile("telephone.html", { root: "pages" });
 });
 
-// Chemin pour renvoyer la liste des chansons possibles au format JSON
+// Chemin pour envoyer la liste de chansons possibles
 app.get("/chansonsPossibles", (req, res) => {
     const musiques = []
     fs.readdirSync("./infoChansons/").forEach(file => {
@@ -143,19 +114,18 @@ app.get("/chansonsPossibles", (req, res) => {
     res.json(musiques);
 });
 
-// Chemin pour renvoyer les informations sur une chanson spécifique
+// Chemin pour envoyer les informations sur les chansons
 app.get("/chanson/:nom", (req, res) => {
     const nom = req.params.nom
     res.sendFile(nom, { root: 'infoChansons' });
 });
 
-// Chemin pour renvoyer les fichiers audio
+// Chemin pour envoyer les audios
 app.get("/audio/:nom", (req, res) => {
     const nom = req.params.nom
     res.sendFile(nom, { root: 'audio' });
 });
 
-// Démarrage du serveur Express
 app.listen(port, () => {
     console.log(`Serveur écoute sur le port ${port}.`);
 });
