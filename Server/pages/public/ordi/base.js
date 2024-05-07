b973add90c0b895f6ccec9c4341b8920c4e0a5d8
manndonk <manndonK@gmail.com>
2024-05-06 20:30:51 -0400

    Revert "Update base.js"
    This reverts commit 712b602f86d46dc2fccb13d02792bf41c4ef61da.

--- Server/pages/public/ordi/base.js
+++ Server/pages/public/ordi/base.js
@@ -1,22 +1,14 @@
-// Récupération du canvas et du contexte 2D pour dessiner
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
-// Désactivation du lissage d'image pour un rendu pixelisé
 ctx.webkitImageSmoothingEnabled = false;
 ctx.mozImageSmoothingEnabled = false;
 ctx.imageSmoothingEnabled = false;
 
-// Récupération de la largeur et de la hauteur du canvas
 const canvasWidth = ctx.canvas.width;
 const canvasHeight = ctx.canvas.height;
 
-// Largeur d'une voie (lane) dans le jeu
 const laneWidth = 65;
-
-// Calcul des centres des voies (lanes) pour le placement des flèches
-const laneCenters = [canvasWidth/2 - (3/2)*(laneWidth)]; // Position du centre de la première voie
+const laneCenters = [canvasWidth/2 - (3/2)*(laneWidth)];
 for (i = 1; i < 4; i++) {
-    // Calcul de la position du centre de chaque voie suivante en ajoutant la largeur de la voie
     laneCenters.push(laneCenters[i-1] + laneWidth);
 }
